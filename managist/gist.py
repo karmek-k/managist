2901440641d362fc0adb9492823230734e10b96f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-25 10:10:32 +0100

    Implement Gist.save function

--- managist/gist.py
+++ managist/gist.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from github.MainClass import Github
+from github.InputFileContent import InputFileContent
 
 from managist.string_wrapper import StringWrapper
 
@@ -9,13 +10,23 @@ class Gist:
     def __init__(self,
                  github: Github,
                  title: str,
-                 content: Optional[str]=None,
+                 content: str,
                  wrapper: Optional[StringWrapper]=None) -> None:
         self.github = github
         self.title = title
+        self.github_gist = None
 
         if wrapper:
             self.content = wrapper.wrap(content)
         else:
             self.content = content
-    
+
+    def save(self, public: bool=False) -> None:
+        file_dict = {'gist': InputFileContent(self.content, self.title)}
+        user = self.github.get_user()
+
+        if not self.github_gist:
+            self.github_gist = user.create_gist(public, files=file_dict)
+        else:
+            self.github_gist.edit(files=file_dict)
+
