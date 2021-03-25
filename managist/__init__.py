89736cf6ea142c605821481d39a2b3a8f2196baf
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-03-25 08:53:28 +0100

    Create a gist factory function in Managist

--- managist/__init__.py
+++ managist/__init__.py
@@ -1,14 +1,23 @@
+from typing import Optional
+
 from github import Github
-from github.Gist import Gist as GithubGist
 
 from managist.gist import Gist
+from managist.string_wrapper import StringWrapper, WRAP_THRESHOLD
 
 
 class Managist:
     def __init__(self, access_token: str) -> None:
         self.github = Github(access_token)
-        self.user = self.github.get_user()
 
         # test if the token is correct
-        self.user.get_repos().totalCount
+        self.github.get_user().get_repos().totalCount
+    
+    def create_gist(self,
+                    title: str,
+                    content: Optional[str]=None,
+                    wrap: bool=False,
+                    wrap_threshold: int=WRAP_THRESHOLD):
+        wrapper = StringWrapper(wrap_threshold) if wrap else None
 
+        return Gist(self.github, title, content, wrapper)
